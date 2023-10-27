466c562d089395feef55831ac58451f75754fcd4
Madness676 <117950457+Madness676@users.noreply.github.com>
2023-10-28 07:38:22 +0800

    AutoPiano3-2.lua

--- AutoPiano3-2.lua
+++ AutoPiano3-2.lua
@@ -1,35 +1,24 @@
 local function callback(Text)
-
-if Text == "Yes" then
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/MADNESSTEST/Pqoeirnfjw/main/AP3-5.lua", true))()
-
-elseif Text == ("No") then
-
-print ("3.2 not loading...")
-
-end
-
+    if Text == "Yes" then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/MADNESSTEST/Pqoeirnfjw/main/AP3-5.lua", true))()
+    elseif Text == "No" then
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+Title = "3.2 not loading...",
+Text = "Check Panda Hub at YT for more updates", 
+Duration = 3
+})
+    end
 end
 
 local NotificationBindable = Instance.new("BindableFunction")
-
 NotificationBindable.OnInvoke = callback
 
 game.StarterGui:SetCore("SendNotification", {
-
-Title = "Auto Piano Update";
-
-Text = "Do you want to open 3.5?";
-
-Icon = "";
-
-Duration = 5;
-
-Button1 = "Yes";
-
-Button2 = "No";
-
-Callback = NotificationBindable;
-
-}}
+    Title = "Auto Piano Update",
+    Text = "Do you want to open 3.5?",
+    Icon = "",
+    Duration = 5,
+    Button1 = "Yes",
+    Button2 = "No",
+    Callback = NotificationBindable,
+})
